ebb54d0df437039240f84d705b707e20281a1912
Leonard Blier <leonard.blier@gmail.com>
2016-06-07 11:28:15 +0200

    re-organisation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='convnetskeras',
+      version='0.1',
+      description='Pre-trained convnets in Keras',
+      author='Leonard Blier',
+      author_email='leonard.blier@ens.fr',
+      packages=['convnetskeras']
+     )
+
