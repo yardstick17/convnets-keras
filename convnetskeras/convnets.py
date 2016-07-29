20b7408f12a7c60842d2b5a6a76cdb3b617c2477
EloiZ <eloi.zablocki@gmail.com>
2016-07-29 15:32:52 +0200

    PEP8 and change dog synset

--- convnetskeras/convnets.py
+++ convnetskeras/convnets.py
@@ -1,33 +1,29 @@
-import numpy as np
-
 from keras.models import Sequential, Model
 from keras.layers import Flatten, Dense, Dropout, Reshape, Permute, Activation, \
     Input, merge
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
+import numpy as np
+from scipy.misc import imread, imresize, imsave
 
 from convnetskeras.customlayers import convolution2Dgroup, crosschannelnormalization, \
     splittensor, Softmax4D
-
 from convnetskeras.imagenet_tool import synset_to_id, id_to_synset,synset_to_dfs_ids
-
-from scipy.misc import imread, imresize, imsave
-
 
 
 def convnet(network, weights_path=None, heatmap=False,
             trainable=None):
     """
     Returns a keras model for a CNN.
-    
-    BEWARE !! : Since the different convnets have been trained in different settings, they don't take 
-    data of the same shape. You should change the arguments of preprocess_img_batch for each CNN : 
+
+    BEWARE !! : Since the different convnets have been trained in different settings, they don't take
+    data of the same shape. You should change the arguments of preprocess_img_batch for each CNN :
     * For AlexNet, the data are of shape (227,227), and the colors in the RGB order (default)
     * For VGG16 and VGG19, the data are of shape (224,224), and the colors in the BGR order
 
     It can also be used to look at the hidden layers of the model.
 
-    It can be used that way : 
+    It can be used that way :
     >>> im = preprocess_img_batch(['cat.jpg'])
 
     >>> # Test pretrained model
@@ -46,7 +42,7 @@ def convnet(network, weights_path=None, heatmap=False,
 
     heatmap: bool
         Says wether the fully connected layers are transformed into Convolution2D layers,
-        to produce a heatmap instead of a 
+        to produce a heatmap instead of a
 
 
     Returns
@@ -58,7 +54,7 @@ def convnet(network, weights_path=None, heatmap=False,
         Dict of feature layers, asked for in output_layers.
     """
 
-    
+
     # Select the network
     if network == 'vgg_16':
         convnet_init = VGG_16
@@ -89,15 +85,15 @@ def convnet(network, weights_path=None, heatmap=False,
 
     return model
 
-    
+
 
 
 def VGG_16(weights_path=None, heatmap=False):
     model = Sequential()
     if heatmap:
-        model.add(ZeroPadding2D((1,1),input_shape=(3,None,None))) 
+        model.add(ZeroPadding2D((1,1),input_shape=(3,None,None)))
     else:
-        model.add(ZeroPadding2D((1,1),input_shape=(3,224,224))) 
+        model.add(ZeroPadding2D((1,1),input_shape=(3,224,224)))
     model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_1'))
     model.add(ZeroPadding2D((1,1)))
     model.add(Convolution2D(64, 3, 3, activation='relu', name='conv1_2'))
@@ -221,17 +217,17 @@ def VGG_19(weights_path=None,heatmap=False):
 
     return model
 
-    
+
 
 def AlexNet(weights_path=None, heatmap=False):
     if heatmap:
         inputs = Input(shape=(3,None,None))
     else:
         inputs = Input(shape=(3,227,227))
-    
+
     conv_1 = Convolution2D(96, 11, 11,subsample=(4,4),activation='relu',
                            name='conv_1')(inputs)
-    
+
     conv_2 = MaxPooling2D((3, 3), strides=(2,2))(conv_1)
     conv_2 = crosschannelnormalization(name="convpool_1")(conv_2)
     conv_2 = ZeroPadding2D((2,2))(conv_2)
@@ -256,7 +252,7 @@ def AlexNet(weights_path=None, heatmap=False):
         Convolution2D(128,3,3,activation="relu",name='conv_5_'+str(i+1))(
             splittensor(ratio_split=2,id_split=i)(conv_5)
         ) for i in range(2)], mode='concat',concat_axis=1,name="conv_5")
-    
+
     dense_1 = MaxPooling2D((3, 3), strides=(2,2),name="convpool_5")(conv_5)
 
     if heatmap:
@@ -269,11 +265,11 @@ def AlexNet(weights_path=None, heatmap=False):
         dense_1 = Dense(4096, activation='relu',name='dense_1')(dense_1)
         dense_2 = Dropout(0.5)(dense_1)
         dense_2 = Dense(4096, activation='relu',name='dense_2')(dense_2)
-        dense_3 = Dropout(0.5)(dense_2)  
+        dense_3 = Dropout(0.5)(dense_2)
         dense_3 = Dense(1000,name='dense_3')(dense_3)
         prediction = Activation("softmax",name="softmax")(dense_3)
 
-    
+
     model = Model(input=inputs, output=prediction)
 
     if weights_path:
@@ -286,18 +282,18 @@ def AlexNet(weights_path=None, heatmap=False):
 
 def preprocess_image_batch(image_paths, img_size=None, crop_size=None, color_mode="rgb", out=None):
     img_list = []
-    
+
     for im_path in image_paths:
         img = imread(im_path, mode='RGB')
         if img_size:
             img = imresize(img,img_size)
-            
+
         img = img.astype('float32')
         # We permute the colors to get them in the BGR order
         if color_mode=="bgr":
             img[:,:,[0,1,2]] = img[:,:,[2,1,0]]
         # We normalize the colors with the empirical means on the training set
-        img[:, :, 0] -= 123.68 
+        img[:, :, 0] -= 123.68
         img[:, :, 1] -= 116.779
         img[:, :, 2] -= 103.939
         img = img.transpose((2, 0, 1))
@@ -305,7 +301,7 @@ def preprocess_image_batch(image_paths, img_size=None, crop_size=None, color_mod
         if crop_size:
             img = img[:,(img_size[0]-crop_size[0])//2:(img_size[0]+crop_size[0])//2
                       ,(img_size[1]-crop_size[1])//2:(img_size[1]+crop_size[1])//2]
-            
+
         img_list.append(img)
 
     img_batch = np.stack(img_list, axis=0)
@@ -319,23 +315,20 @@ def preprocess_image_batch(image_paths, img_size=None, crop_size=None, color_mod
 
 
 if __name__ == "__main__":
-    ### Here is a script to compute the heatmap of the cars synsets.
-    ## We find the synsets corresponding to cars on ImageNet website
+    ### Here is a script to compute the heatmap of the dog synsets.
+    ## We find the synsets corresponding to dogs on ImageNet website
     s = "n02084071"
     ids = synset_to_dfs_ids(s)
     # Most of the synsets are not in the subset of the synsets used in ImageNet recognition task.
     ids = np.array([id for id in ids if id != None])
-    
+
     im = preprocess_image_batch(['examples/dog.jpg'],color_mode="bgr")
 
     # Test pretrained model
     sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
     model = convnet('alexnet',weights_path="weights/alexnet_weights.h5", heatmap=True)
     model.compile(optimizer=sgd, loss='mse')
-    
-    
+
+
     out = model.predict(im)
     heatmap = out[0,ids,:,:].sum(axis=0)
-    
-
-    
